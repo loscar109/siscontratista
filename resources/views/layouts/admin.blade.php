f87e9425efe2431893a33ea8307fe04cfa417980
loscar109 <charly_groso_64@hotmail.com>
2018-12-02 17:54:06 -0300

    cambios hechos de una version a otras

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,13 +3,19 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>ADVentas | www.incanatoit.com</title>
+    <title>Tecnicas y Servicios</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.5 -->
     <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/css/bootstrap-select.min.css">
+
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{asset('css/font-awesome.css')}}">
+    <link rel="stylesheet" href="{{asset('font/css/fontawesome.css')}}">
+    <link rel="stylesheet" href="{{asset('font/css/fontawesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('font/css/regular.css')}}">
+    <link rel="stylesheet" href="{{asset('font/css/all.css')}}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{asset('css/AdminLTE.min.css')}}">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
@@ -25,11 +31,11 @@
       <header class="main-header">
 
         <!-- Logo -->
-        <a href="index2.html" class="logo">
+        <a href="/" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b>AD</b>V</span>
+          <span class="logo-mini"><b>Ty</b>S</span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>ADVentas</b></span>
+          <span class="logo-lg"><b>TyServicios</b></span>
         </a>
 
         <!-- Header Navbar: style can be found in header.less -->
@@ -46,16 +52,16 @@
               <!-- User Account: style can be found in dropdown.less -->
               <li class="dropdown user user-menu">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <small class="bg-red">Online</small>
-                  <span class="hidden-xs">Juan Carlos Arcila Díaz</span>
+                  <!--<small class="bg-red">Online</small>-->
+                  <span class="hidden-xs">Jaime Ramon Villalba</span>
                 </a>
                 <ul class="dropdown-menu">
                   <!-- User image -->
                   <li class="user-header">
                     
                     <p>
-                      www.incanatoit.com - Desarrollando Software
-                      <small>www.youtube.com/jcarlosad7</small>
+                      Tecnicas y Servicios
+                      <small>Jefe de Sucursal</small>
                     </p>
                   </li>
                   
@@ -63,7 +69,7 @@
                   <li class="user-footer">
                     
                     <div class="pull-right">
-                      <a href= "{{ route('logout') }}" class="btn btn-default btn-flat">Cerrar</a>
+                      <a href="{{url('/logout')}}" class="btn btn-default btn-flat">Cerrar</a>
                     </div>
                   </li>
                 </ul>
@@ -82,65 +88,109 @@
                     
           <!-- sidebar menu: : style can be found in sidebar.less -->
           <ul class="sidebar-menu">
+
+
             <li class="header"></li>
             
             <li class="treeview">
               <a href="#">
-                <i class="fa fa-laptop"></i>
-                <span>Almacén</span>
+                <i class="fa fa-users"></i>
+                <span>Empleados</span>
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="almacen/articulo"><i class="fa fa-circle-o"></i> Artículos</a></li>
-                <li><a href="almacen/categoria"><i class="fa fa-circle-o"></i> Categorías</a></li>
+                <li><a href="{{asset('empleado/gestion')}}"><i class="fas fa-circle"></i> Gestionar Empleado</a></li>
               </ul>
             </li>
             
+
+
+
             <li class="treeview">
               <a href="#">
-                <i class="fa fa-th"></i>
-                <span>Compras</span>
+                <i class="fa fa-car"></i>
+                <span>Vehiculos</span>
                  <i class="fa fa-angle-left pull-right"></i>
               </a>
-              <ul class="treeview-menu">
-                <li><a href="compras/ingreso"><i class="fa fa-circle-o"></i> Ingresos</a></li>
-                <li><a href="compras/proveedor"><i class="fa fa-circle-o"></i> Proveedores</a></li>
-              </ul>
-            </li>
-            <li class="treeview">
-              <a href="#">
-                <i class="fa fa-shopping-cart"></i>
-                <span>Ventas</span>
-                 <i class="fa fa-angle-left pull-right"></i>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="ventas/venta"><i class="fa fa-circle-o"></i> Ventas</a></li>
-                <li><a href="ventas/cliente"><i class="fa fa-circle-o"></i> Clientes</a></li>
-              </ul>
+               <ul class="treeview-menu">
+                  <li><a href="{{asset('vehiculo/gestion')}}"><i class="fas fa-circle"></i> Gestionar Vehiculos</a></li>
+              </ul> 
             </li>
                        
             <li class="treeview">
               <a href="#">
-                <i class="fa fa-folder"></i> <span>Acceso</span>
+                <i class="fa fa-file"></i> <span>Tareas</span>
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="configuracion/usuario"><i class="fa fa-circle-o"></i> Usuarios</a></li>
+                <li><a href="{{asset('tarea/gestion')}}"><i class="fas fa-circle"></i> Gestionar Tareas</a></li>
+                <li><a href="{{asset('tarea/gestion')}}"><i class="fas fa-circle"></i> Asignar Subtareas</a></li>
                 
               </ul>
             </li>
-             <li>
+
+            <li class="treeview">
+                <a href="#">
+                  <i class="fa fa-folder"></i> <span>Ordenes de Trabajo</span>
+                  <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                  <ul class="treeview-menu">
+                    <li><a href="{{asset('orden/gestion')}}"><i class="fas fa-circle"></i> Gestionar OT</a></li>
+                  
+                  </ul>
+            </li>
+
+            <li class="treeview">
+              <a href="#">
+                <i class="fa fa-wrench"></i> <span>Moviles</span>
+                <i class="fa fa-angle-left pull-right"></i>
+              </a>
+                <ul class="treeview-menu">
+                  <li><a href="{{asset('movil/gestion')}}"><i class="fas fa-circle"></i> Asginación de Moviles</a></li>
+                </ul>
+            </li>
+
+
+
+
+            <li class="treeview">
+                <a href="#">
+                  <i class="fa fa-cogs"></i>
+                  <span>Configuraciones</span>
+                   <i class="fa fa-angle-left pull-right"></i>
+                </a>
+                <ul class="treeview-menu">
+                  <li><a href="{{asset('configuracion/pais')}}"><i class="fas fa-circle"></i> Paises</a></li>
+                  <li><a href="{{asset('configuracion/provincia')}}"><i class="fas fa-circle"></i> Provincias</a></li>
+                  <li><a href="{{asset('configuracion/localidad')}}"><i class="fas fa-circle"></i> Localidades</a></li>
+                  <li><a href="{{asset('configuracion/estado_civil')}}"><i class="fas fa-circle"></i> Estado Civil</a></li>
+                  <li><a href="{{asset('configuracion/tipo_telefono')}}"><i class="fas fa-circle"></i> Tipo de Teléfono</a></li>
+                  <li><a href="{{asset('configuracion/telefono')}}"><i class="fas fa-circle"></i> Teléfono</a></li>
+                  <li><a href="{{asset('configuracion/tipo_vehiculo')}}"><i class="fas fa-circle"></i> Tipo de vehiculo</a></li>
+                  <li><a href="{{asset('configuracion/marca')}}"><i class="fas fa-circle"></i> Marca de Vehiculo</a></li>
+                  <li><a href="{{asset('configuracion/modelo')}}"><i class="fas fa-circle"></i> Modelo de Vehiculo</a></li>
+
+                  <li><a href="{{asset('configuracion/estado_vehiculo')}}"><i class="fas fa-circle"></i> Estado de Vehiculo</a></li>
+
+
+
+                </ul>
+              </li>
+
+
+
+            <!-- <li>
               <a href="#">
                 <i class="fa fa-plus-square"></i> <span>Ayuda</span>
                 <small class="label pull-right bg-red">PDF</small>
               </a>
-            </li>
-            <li>
+            </li>-->
+            <!--<li>
               <a href="#">
                 <i class="fa fa-info-circle"></i> <span>Acerca De...</span>
                 <small class="label pull-right bg-yellow">IT</small>
               </a>
-            </li>
+            </li>-->
                         
           </ul>
         </section>
@@ -161,21 +211,22 @@
           <div class="row">
             <div class="col-md-12">
               <div class="box">
+
                 <div class="box-header with-border">
-                  <h3 class="box-title">Sistema de Ventas</h3>
-                  <div class="box-tools pull-right">
-                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-                    
-                    <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                  </div>
+                  @yield('titulo')
+                  {{--<h3 class="box-title">Sistema de Control, Cierre y Seguimiento de Tareas</h3>--}}
+                  
                 </div>
+
                 <!-- /.box-header -->
                 <div class="box-body">
                   	<div class="row">
 	                  	<div class="col-md-12">
-		                          <!--Contenido-->
+		                        <!--Contenido-->
+                              
                                 @yield('contenido')
-		                          <!--Fin Contenido-->
+
+		                        <!--Fin Contenido-->
                            </div>
                         </div>
 		                    
@@ -191,17 +242,20 @@
       <!--Fin-Contenido-->
       <footer class="main-footer">
         <div class="pull-right hidden-xs">
-          <b>Version</b> 2.3.0
+          <b>Version</b> 1.0
         </div>
-        <strong>Copyright &copy; 2015-2020 <a href="www.incanatoit.com">IncanatoIT</a>.</strong> All rights reserved.
+        <strong>Copyright &copy; 2018 <a href="www.charly_groso_64@hotmail.com">Carlos Villalba</a>.</strong> Todos los derechos reservados.
       </footer>
 
       
     <!-- jQuery 2.1.4 -->
     <script src="{{asset('js/jQuery-2.1.4.min.js')}}"></script>
+    @stack('scripts')
     <!-- Bootstrap 3.3.5 -->
     <script src="{{asset('js/bootstrap.min.js')}}"></script>
-    <!-- AdminLTE App -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/js/bootstrap-select.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.2/js/i18n/defaults-*.min.js"></script>
+        <!-- AdminLTE App -->
     <script src="{{asset('js/app.min.js')}}"></script>
     
   </body>
